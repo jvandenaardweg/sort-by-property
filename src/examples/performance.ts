abb49db85691c2b17778ea3661d19b5cbcf171be
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2022-11-11 15:32:20 +0100

    chore: fix ci on performance.ts

--- src/examples/performance.ts
+++ src/examples/performance.ts
@@ -24,42 +24,6 @@ const blogPost: BlogPost = {
   },
 };
 
-const blogPosts: BlogPost[] = [
-  {
-    id: 1,
-    title: 'A title',
-    author: {
-      id: 10,
-      name: 'Joe',
-      location: {
-        city: 'Copenhagen',
-      },
-    },
-  },
-  {
-    id: 2,
-    title: 'Better title',
-    author: {
-      id: 20,
-      name: 'Ben',
-      location: {
-        city: 'Bangkok',
-      },
-    },
-  },
-  {
-    id: 3,
-    title: 'Cake title',
-    author: {
-      id: 30,
-      name: 'Alice',
-      location: {
-        city: 'Amsterdam',
-      },
-    },
-  },
-];
-
 const array = Array<BlogPost>(10000000).fill(blogPost);
 
 console.time('perf');
